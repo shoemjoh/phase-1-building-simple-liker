ae9c969ae226d5264b1cac028aa59c0de670821b
John <shoemjoh@gmail.com>
2023-11-23 12:43:04 -0500

    liking lab

--- main.js
+++ main.js
@@ -4,6 +4,36 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const hearts = document.getElementsByClassName("like");
+
+// Iterate through each element with the class "like"
+for (let heart of hearts) {
+  // Add a click event listener to each element
+  heart.addEventListener('click', function () {
+    mimicServerCall()
+      .then(function (res) {
+        if (heart.innerText.includes(EMPTY_HEART)) {
+          heart.innerHTML = `<span class="like-glyph">${FULL_HEART}</span>`;
+          heart.classList.add('activated-heart');
+        } else {
+          heart.innerHTML = `Like! <span class="like-glyph">${EMPTY_HEART}</span>`;
+          heart.classList.remove('activated-heart');
+        }
+        console.log(res); // Log the response after the promise resolves
+      })
+      .catch(function (error) {
+        let hideClass = document.getElementsByClassName('hidden');
+        // Loop through the collection and remove the 'hidden' class from each element
+        for (let element of hideClass) {
+          element.classList.remove('hidden');
+          setTimeout(function () {
+            element.classList.add('hidden');
+          }, 3000);
+        }
+      });
+  });
+}
+
 
 
 
@@ -11,9 +41,9 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
